93bbbadda64438ef0bc1df0da27d3ff0344f63f6
Lucas Fernandes <lucasfernandes.rf@gmail.com>
2025-02-04 21:00:46 -0300

    Validar Conexao

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 import app from './src/app.js'
+
+import conexao from './infra/conexao.js'
+
 const PORT = 3000
 
-//escutar a porta 3001
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+//estabelecer conexao
+
+conexao.connect((erro) => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log("Conexao realizada com sucesso!")
+        //escutar a porta 3001
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+        })
+    }
+
 })
+
+
+
