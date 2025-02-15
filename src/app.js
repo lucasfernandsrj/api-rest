532de0f36b221f2138ffca71d21a01e14595e0aa
Lucas Fernandes <lucasfernandes.rf@gmail.com>
2025-02-15 11:51:03 -0300

    MVC Controller

--- src/app.js
+++ src/app.js
@@ -1,83 +1,16 @@
 import express from 'express'
-import conexao from '../infra/conexao.js'
+import SelecaoController from './app/controllers/SelecaoController.js'
 
 const app = express()
 
 //indicar para o express ler body com json!
 app.use(express.json()) 
 
-//retorna o objeto por id
-function buscarSelecaoPorId(id){
-    return selecoes.filter( selecao => selecao.id == id )
-}
-
-//pegar a posicao ou index do elemento no array por id
-function buscarIndexSelecao(id){
-    return selecoes.findIndex( selecao => selecao.id == id )
-}
-
 //ROTAS
-app.get('/selecoes/:id', (req, res) => {
-    //res.status(200).json(buscarSelecaoPorId(req.params.id))
-    const id = req.params.id
-    const sql = "SELECT * FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (erro, resultado) => {
-        const linha = resultado[0]
-        if(erro){
-            res.status(404).json({ 'erro': erro })
-        }else{
-            res.status(200).json(linha)
-        }
-    })
-})
-
-app.get('/selecoes', (req, res) => {
-    //res.status(200).send(selecoes)
-    const sql = "SELECT * FROM selecoes;"
-    conexao.query(sql, (erro, resultado) => {
-        if(erro){
-            res.status(404).json({ 'erro': erro })
-        }else{
-            res.status(200).json(resultado)
-        }
-    })
-})
-
-app.post('/selecoes', (req, res) => {
-    const selecao = req.body
-    const sql = "INSERT INTO selecoes SET ?;"
-    conexao.query(sql, selecao, (erro, resultado) => {
-        if(erro){
-            res.status(400).json({ 'erro': erro })
-        }else{
-            res.status(201).json(resultado)
-        }
-    })
-})
-
-app.delete('/selecoes/:id', (req, res) => {
-    const id = req.params.id
-    const sql = "DELETE FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (erro, resultado) => {
-        if(erro){
-            res.status(404).json({ 'erro': erro })
-        }else{
-            res.status(200).json(resultado)
-        }
-    })
-})
-
-app.put('/selecoes/:id', (req, res) => {
-    const id = req.params.id
-    const selecao = req.body
-    const sql = "UPDATE selecoes SET ? WHERE id=?;"
-    conexao.query(sql, [selecao,id], (erro, resultado) => {
-        if(erro){
-            res.status(400).json({ 'erro': erro })
-        }else{
-            res.status(200).json(resultado)
-        }
-    })
-})
+app.get('/selecoes', SelecaoController.index)
+app.get('/selecoes/:id', SelecaoController.show)
+app.post('/selecoes', SelecaoController.store)
+app.put('/selecoes/:id', SelecaoController.update)
+app.delete('/selecoes/:id', SelecaoController.delete)
 
 export default app
